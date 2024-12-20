d0731d9716debf79987ac3ab41bc29e11d1034af
sandu-motelica <motelica.sb@gmail.com>
2024-12-20 02:31:14 +0200

    add-cmd-args-and-dynamic-dimensions

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from scripts.svg_parser import Parser
 from scripts.renderer import Renderer
+import sys
+
+
+def convert(input, output):
+    """
+    Converts an SVG file to a PNG file.
+
+    Args:
+        input (str): Path to the input SVG file.
+        output (str): Path to the output PNG file.
+
+    """
+    p = Parser(input)
+    width, height = p.extract_dimensions()
+    elements = p.parse()
+    r = Renderer(width, height)
+    r.draw_elements(elements)
+    r.save_PNG(output)
+
 
 if __name__ == '__main__':
-    # help(Parser)
+    if len(sys.argv) != 3:
+        print("Invalid arguments. Usage : python main.py <input_file> <output_file>")
+        exit(1)
 
-    p = Parser("data\\example.svg")
-    elements = p.parse()
-    r = Renderer(400, 400)
-    r.draw_elements(elements)
-    r.save_PNG("output.png")
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    convert(input_file, output_file)
